d0d519167f4423328b0fb90f51bb5dec8c6b77b8
Jackyzzk <46434291+Jackyzzk@users.noreply.github.com>
2020-09-15 03:21:42 +0800

    shop

---
+++ git-help.py
@@ -0,0 +1,11 @@
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M master
+# git remote add origin https://github.com/Jackyzzk/Django-Project.git
+# git push -u origin master
+
+
+# git remote add origin https://github.com/Jackyzzk/Django-Project.git
+# git branch -M master
+# git push -u origin master
